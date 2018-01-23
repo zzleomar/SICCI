ce0485e952fe0e349cbdab714b9319730feb4862
zzleomar <dmy.leomar@gmail.com>
2018-01-23 09:01:47 -0430

    Merge brvgcganch 'master' into otra
    dfgdfg

---
+++ app/Http/Controllers/AdministradorController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Carbon\Carbon;
+use App\Producto;
+use App\Carga;
+use App\Http\Requests;
+
+class AdministradorController extends Controller
+{
+	public function __construct(){
+        Carbon::resetToStringFormat();
+      Carbon::setLocale('es');
+      date_default_timezone_set('America/Caracas');
+    }
+
+    public function index(){
+    	$actual=Carbon::now();
+    	$actual2=Carbon::now();
+    	$actual2->subDays(7);
+    	$recientes=Carga::recientes($actual2,$actual)->paginate(4);
+        $chartjs = app()->chartjs
+        ->name('lineChartTest')
+        ->type('line')
+        ->size(['width' => 400, 'height' => 200])
+        ->labels(['10', '11', '12', '13', '14', '15', '16'])
+        ->datasets([
+            [
+                "label" => "Disponibilidad de Estacionamiento",
+                'backgroundColor' => "rgba(38, 185, 154, 0.31)",
+                'borderColor' => "rgba(38, 185, 154, 0.7)",
+                "pointBorderColor" => "rgba(38, 185, 154, 0.7)",
+                "pointBackgroundColor" => "rgba(38, 185, 154, 0.7)",
+                "pointHoverBackgroundColor" => "#fff",
+                "pointHoverBorderColor" => "rgba(220,220,220,1)",
+                'data' => [65, 59, 80, 81, 56, 55, 40],
+            ]
+        ])
+        ->options([]);
+    	//  //para llegar al nombre y apellido
+    	return view("administrador.index", compact('chartjs'))->with('recientes',$recientes);
+    }
+}
---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,79 @@
+@extends('layouts.appT')
+
+@section('content')
+<div class="container" id="container-sesion">
+    <div class="row" >
+        <div class="col-sm-6 col-md-4 col-md-offset-4" id="containersesion" >
+
+            <div class="card card-default py-3" style="border-radius: 15px;">  
+        <div class="panel-heading text-center" style="font-weight: 700; margin-bottom: 5px;">INICIO DE SESIÓN</div>
+            <div class="card-heading d-flex justify-content-center center-block">
+                <img src="{{ asset('img/icon-user-default.png') }}" alt="Usuario" class="img-circle img-responsive " style="    display: block; margin: auto; padding: 10px;">
+            </div>  
+            <form class="form-horizontal" method="POST" action="{{ route('login') }}">
+                            {{ csrf_field() }}
+      <div class="form-group">
+        <label for="inputEmail3" class="col-sm-4 control-label">Nro. Cédula</label>
+        <div class="form-group{{ $errors->has('cedula') ? ' has-error' : '' }} col-sm-8">
+          <input type="text" class="form-control" id="inputEmail3" name="cedula" placeholder="Cédula">
+          @if ($errors->has('cedula'))
+                <span class="help-block">
+                    <strong>{{ $errors->first('cedula') }}</strong>
+                </span>
+            @endif
+        </div>
+      </div>
+      <div class="form-group">
+        <label for="inputPassword3" class="col-sm-4 control-label">Contraceña</label>
+        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }} col-sm-8">
+          <input type="password" class="form-control" id="inputPassword3" placeholder="Password" name="password">
+          @if ($errors->has('password'))
+            <span class="help-block">
+                <strong>{{ $errors->first('password') }}</strong>
+            </span>
+        @endif
+        </div>
+      </div>
+      <div class="form-group">
+        <div class="col-sm-offset-2 col-sm-10">
+          <div class="checkbox">
+            <label>
+                <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Recuerdame
+            </label>
+          </div>
+        </div>
+      </div>
+      <div class="form-group">
+        <div class="col-sm-offset-2 col-sm-10">
+          <button type="submit" class="btn btn-default">ENTRAR</button>
+          <a class="btn btn-link text-danger" href="{{ route('password.request') }}">
+            ¿Olvido su Contraseña?
+        </a>
+        </div>
+      </div>
+    </form>
+    </div></div>
+
+</div>
+        
+
+    
+    
+    
+
+
+@endsection
+@section('scripts')
+<script type="text/javascript">
+    
+$(document).ready(function(){
+
+  var altura = $(document).height();
+  altura=(altura/2)-300;
+  altura=altura+"px";
+  $("#container-sesion").css("margin-top",altura);
+
+});
+
+</script>
+@endsection
